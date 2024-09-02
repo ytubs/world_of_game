f8d1f0a2670bff5143550ec79b2a3a2ac47923b9
Yoni <yoni@toubul.eu>
2024-09-02 16:42:06 +0300

    added input validation

--- app.py
+++ app.py
@@ -1,7 +1,24 @@
+
 def welcome():
     print(f'Hi {input("Enter your Username: ")} and welcome to the World of Games: The Epic Journey')
 
 def start_play():
-    games = ['1. Memory Game', '2. Guess Game', '3. Currency Roulette']
-    selection = games[int(input(f'Select Game (enter number 1-3): \n{", ".join(games)}\n')) - 1]
+    games = ['Memory Game', 'Guess Game', 'Currency Roulette']
+    while True:
+        numbered_games= '\n'.join(f'{index + 1}. {game}' for index, game in enumerate(games))
+        selection = input(f'Select Game (enter number 1-{len(games)}): \n{numbered_games}\n')
+        if selection_validation(selection,games):
+            break
+        print("invalid input, try again")
+        print('''\n\n\n------------\n\n\n''')
     difficulty = input('Enter disired difficulty (1-5): \n')
+
+def selection_validation(selection, games):
+    try:
+        index = int(selection) - 1
+        return 0 <= index < len(games)-1
+    except ValueError:
+        return False
+
+
+
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from app import start_play, welcome
+from app import start_play, welcome, selection_validation
+
 
 welcome()
 start_play()
