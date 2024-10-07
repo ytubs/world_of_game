437cefc6575d6466d491a272c6e74d456b64cb61
Yoni <yoni@toubul.eu>
2024-10-07 12:40:11 +0300

    guess game file pretty much done

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 def welcome():
     print(f'Hi {input("Enter your Username: ")} and welcome to the World of Games: The Epic Journey')
 
-
+selected_difficulty = None
 def start_play():
     games = ['Memory Game', 'Guess Game', 'Currency Roulette']
     max_difficulty = 5
@@ -21,10 +21,10 @@ def number_validation(number, end_of_range):
 
 def get_user_input_validate_with_message(end_of_range, message="Enter selection:\n"):
     while True:
-        user_input = input(message)
+        user_input = int(input(message))
         if number_validation(user_input, end_of_range):
             break
         print("invalid input, try again")
         print('''\n\n\n------------\n\n\n''')
 
-    return user_input
+    return int(user_input)
--- guess_game.py
+++ guess_game.py
@@ -1,24 +1,41 @@
 import random
+import time
+
 from app import get_user_input_validate_with_message
+from app import selected_difficulty
 
-difficulty = 4
-
+selected_difficulty = 5
 def generate_random_secret():
-    return random.randint(0,difficulty)
+    print('Generating Ultra Secret random number...')
+    time.sleep(2)
+    print('Done!')
+    time.sleep(1)
+    return random.randint(0,selected_difficulty)
 
 def get_guess_from_user():
-    return get_user_input_validate_with_message(difficulty,f"Submit your guess! Must be between 0 to {difficulty}")
+    return get_user_input_validate_with_message(selected_difficulty,f"Submit your guess! Must be between 0 to {selected_difficulty}\n")
 
 
 
 
-def compare_input_to_secret():
-    return generate_random_secret() == get_guess_from_user()
-
+def compare_input_to_secret(secret,input):
+    print('Comparing your guess with the super duper secret number...')
+    time.sleep(2)
+    return secret == input
 
 def play():
-    compare_input_to_secret()
+    print('''Welcome to the Guess Game!
+    We will now generating a secret number!
+    ''')
+    secret_number = generate_random_secret()
+    print(secret_number)
+    usr_input = get_guess_from_user()
+    if compare_input_to_secret(secret_number,usr_input):
+        print('you are correct!')
+    else:
+        print('You LOSE!!! HAHAHAH')
 
+play()
 
 
 
