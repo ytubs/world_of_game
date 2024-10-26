424510bdd84ce41518d7bfb7660cba247a6b60ce
Yoni <yoni@toubul.eu>
2024-10-26 14:04:23 +0300

    guess_game modificatation to roll down "difficulty" that will be provided on the main file as "selected_difficulty"

--- guess_game.py
+++ guess_game.py
@@ -2,38 +2,33 @@ import random
 import time
 
 from app import get_user_input_validate_with_message
-from app import selected_difficulty
 
-def generate_random_secret():
+
+def generate_random_secret(difficulty):
     print('Generating Ultra Secret random number...')
-    time.sleep(2)
+    time.sleep(1)
     print('Done!')
     time.sleep(1)
-    return random.randint(0,selected_difficulty)
-
-def get_guess_from_user():
-    return get_user_input_validate_with_message(selected_difficulty,f"Submit your guess! Must be between 0 to {selected_difficulty}\n")
+    return random.randint(0, difficulty)
 
 
+def get_guess_from_user(difficulty):
+    return get_user_input_validate_with_message(difficulty, f"Submit your guess! Must be between 0 to {difficulty}\n", "input is invalid or out of bounds! Try again.")
 
 
-def compare_input_to_secret(secret,input):
+def compare_input_to_secret(secret, user_input):
     print('Comparing your guess with the super duper secret number...')
-    time.sleep(2)
-    return secret == input
+    time.sleep(1)
+    return secret == user_input
 
-def play():
+
+def play(difficulty):
     print('''Welcome to the Guess Game!
     We will now generating a secret number!
     ''')
-    secret_number = generate_random_secret()
-    print(secret_number)
-    usr_input = get_guess_from_user()
-    if compare_input_to_secret(secret_number,usr_input):
-        print('you are correct!')
+    secret_number = generate_random_secret(difficulty)
+    usr_input = get_guess_from_user(difficulty)
+    if compare_input_to_secret(secret_number, usr_input):
+        print('you are correct! You WIN!')
     else:
-        print('You LOSE!!! HAHAHAH')
-
-
-
-
+        print('You LOSE!!!')
